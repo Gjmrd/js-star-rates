13ccad82b78d96e26dc7840c0c59474610488bc8
magomedovgh@gmail.com <magomedovgh@gmail.com>
2018-03-21 18:36:55 +0300

    added server response processing

--- stars.js
+++ stars.js
@@ -21,18 +21,14 @@ args
 function colorStars(rate, starTarget, color){
 
     var stars = starTarget.closest("ul").getElementsByClassName('star');
-
     var starPathStyle;
 
     for (i = 0; i < stars.length; i++){
-
         starPathStyle = stars[i].querySelector("path").style;
-
         if (i < rate)
             starPathStyle.fill = color;
         else 
             starPathStyle.fill = "none";
-        
         starPathStyle.stroke = color;
     }
 }
@@ -42,10 +38,9 @@ function colorStars(rate, starTarget, color){
 
 $('.star').mouseover(function(event)
 {
-    var rate = event.currentTarget.getAttribute('data-star_id')
+    var rate = event.currentTarget.getAttribute('data-star_id');
 
     colorStars(rate, event.target, COLORS.hovered);
-
 });
 
 $('.block-stars').mouseleave(function(event)
@@ -54,7 +49,6 @@ $('.block-stars').mouseleave(function(event)
     var isPredicted = event.currentTarget.closest(".block-stars").getAttribute('data-is_predicted');
 
     colorStars(currentScore, event.target, COLORS.rated)
-
 });
 
 
@@ -66,20 +60,11 @@ $('.star').click(function(event)
     event.preventDefault();
 
     var blockStars = event.currentTarget.closest('.block-stars');
-
     var title_id = blockStars.getAttribute('data-title_id');
     var score = event.currentTarget.getAttribute('data-star_id');
 
-
-    //console.log(score);
-    //console.log(title_id);
-
-
     /* updating info about score in HTML */
-    blockStars.setAttribute('data-stars', score); 
-    event.currentTarget.closest('.block-stars').setAttribute('data-is_predicted', false);
-    colorStars(score, event.target, COLORS.rated);
-    /*
+   
     $.ajax({
         type: 'POST',
         url:'UserRate/rate',
@@ -87,12 +72,18 @@ $('.star').click(function(event)
             title_id: title_id,
             score: score,
         },
-        success: function(callback){
-        //callbackfunction
-            event.currentTarget.closest("div").setAttribute('data-stars', score);
+        success: function(data){
+        	if (data['code'] == "204"){
+        			blockStars.setAttribute('data-stars', score); 
+    		 		blockStars.setAttribute('data-is_predicted', false);
+    		 		colorStars(score, event.target, COLORS.rated);
+        		}
+        	else 
+        		console.log(data['code'] +' : '+data['message']);
+
         },
     });
-    */
+    
 });
 
 
